e96e13074401ff5283d7126f71cabdd4f8dd7a6f
Balazs Fentor <fentorb24@gmail.com>
2025-09-27 00:08:16 +0300

    Fixed a bunch of stuff

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import dotenv
 from atproto import Client
 from atproto import models
 from atproto import client_utils
+from random import choice
+import json
 
-# how often we should check for new notifications
-FETCH_NOTIFICATIONS_DELAY_SEC = 3
+# TO DO: Populate a json with movie quote insults
 
+FETCH_NOTIFICATIONS_DELAY_SEC = 1
 
 def main() -> None:
     dotenv.load_dotenv()
@@ -15,29 +17,36 @@ def main() -> None:
     client = Client()
     client.login(f"{os.getenv("BSKY_USERNAME")}", f"{os.getenv("BSKY_PASSWORD")}")
 
+    data = json.load(open("insults.json", "r"))
+
     # fetch new notifications
     while True:
         last_seen_at = client.get_current_time_iso()
 
         response = client.app.bsky.notification.list_notifications()
         for notification in response.notifications:
-            print(f"{notification.is_read}, {notification.reason}")
             if (notification.is_read == False) and (notification.reason == "mention"):
-                post = models.base.ModelBase(uri=notification.uri, cid=notification.cid,)
-                # print(post)
-                root_post_ref = models.create_strong_ref(post)
-                client.send_post(
-                    text='Get lost',
-                    reply_to=models.AppBskyFeedPost.ReplyRef(parent=root_post_ref, root=root_post_ref)
-                )
-                client.app.bsky.notification.update_seen({'seen_at': last_seen_at})
-                print(":D");
-            
-            else:
-                print("Nothing here...")
-        print("================")
-        sleep(FETCH_NOTIFICATIONS_DELAY_SEC)
+                
+                thread = client.get_post_thread(uri=notification.uri)
+                post = models.base.ModelBase(uri=notification.uri, cid=notification.cid)
 
+                if thread["thread"]["post"]["record"]["reply"] == None:
+                    rootPost = post
+                else:
+                    rootPost = thread["thread"]["post"]["record"]["reply"]["root"]
+
+                parentRef = models.create_strong_ref(post)
+                rootRef = models.create_strong_ref(rootPost)
+
+                client.send_post(
+                    text=choice(list(data.values())),
+                    reply_to=models.AppBskyFeedPost.ReplyRef(parent=parentRef, root=rootRef)
+                )
+                
+                client.app.bsky.notification.update_seen({'seen_at': last_seen_at})
+                print(f"Replied at {last_seen_at}");
+
+        sleep(FETCH_NOTIFICATIONS_DELAY_SEC)
 
 if __name__ == '__main__':
     main()
